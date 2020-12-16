97e13aee8ea6385715d8c488dfc4543340b719cd
DylanThijs <dylan.thijs@student.hu.nl>
2020-12-16 14:00:18 +0100

    get behandelingen kapper

--- app.py
+++ app.py
@@ -17,11 +17,11 @@ conn = mariadb.connect(
     database="kniptoptijd")
 cur = conn.cursor()
 
-@app.route('/', methods=['POST'])
+@app.route('/locatie', methods=['POST'])
 def get_kappers():
-    return post(cur)
+    return post_locatie(cur)
 
-def post(cur):
+def post_locatie(cur):
     parser = reqparse.RequestParser()
     parser.add_argument("locatie")
     args = parser.parse_args()
@@ -32,7 +32,19 @@ def post(cur):
     return jsonify(kapperscollectie)
 
 
+@app.route('/behandeling', methods=['POST'])
+def get_behandeling():
+    return post_behandeling(cur)
 
+def post_behandeling(cur):
+    parser = reqparse.RequestParser()
+    parser.add_argument("idkapper")
+    args = parser.parse_args()
+    print(args['idkapper'])
+    cur.execute("select * from Behandelingen where kapsalonID=?", (args['idkapper'],))
+    behandelingcollectie = [dict((cur.description[i][0], value)
+                for i, value in enumerate(row)) for row in cur.fetchall()]
+    return jsonify(behandelingcollectie)
 #if __name__ == '__main__':
 app.run()
      
